c21d7ce0c094f036257272b6f050949a4ca42c83
Gabor Igloi <gabor.igloi@citrix.com>
2018-02-07 14:40:57 +0000

    VM suspend/resume: start nbd-client for nbd qemu-datapath
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- xc/xenops_server_xen.ml
+++ xc/xenops_server_xen.ml
@@ -300,9 +300,69 @@ module Storage = struct
   let get_disk_by_name = get_disk_by_name
 end
 
+let print_fork_error f =
+  try
+    f ()
+  with Forkhelpers.Spawn_internal_error(stderr, stdout, status) as e ->
+    begin match status with
+      | Unix.WEXITED n ->
+        error "XXXX Forkhelpers.Spawn_internal_error(%s, %s, WEXITED %d)" stderr stdout n;
+        raise e
+      | Unix.WSIGNALED n ->
+        error "XXXX Forkhelpers.Spawn_internal_error(%s, %s, WSIGNALED %d)" stderr stdout n;
+        raise e
+      | Unix.WSTOPPED n ->
+        error "XXXX Forkhelpers.Spawn_internal_error(%s, %s, WSTOPPED %d)" stderr stdout n;
+        raise e
+    end
+
+let start_nbd_client ~unix_socket_path ~export_name =
+  Forkhelpers.execute_command_get_output "/usr/sbin/modprobe" ["nbd"] |> ignore;
+  let is_used ~nbd_device =
+    debug "XXXX checking whether %s is in use" nbd_device;
+    (* First check if the file exists, because "nbd-client -c" returns
+       1 for a non-existent file. *)
+    if not (Sys.file_exists nbd_device) then failwith ("NBD device file " ^ nbd_device ^ " does not exist");
+    try
+      print_fork_error (fun () -> Forkhelpers.execute_command_get_output "/usr/sbin/nbd-client" ["-check"; nbd_device]) |> ignore;
+      true
+    with Forkhelpers.Spawn_internal_error(stderr, stdout, status) as e ->
+      begin match status with
+        | Unix.WEXITED 1 -> false
+        | _ -> raise e
+      end
+  in
+  let find_free_nbd_device () =
+    let rec loop i =
+      let nbd_device = "/dev/nbd" ^ (string_of_int i) in
+      if not (is_used ~nbd_device) then nbd_device else loop (i + 1)
+    in
+    loop 0
+  in
+  let run_nbd_client ~nbd_device =
+    print_fork_error (fun () ->
+        debug "XXXX '/usr/sbin/nbd-client' '-unix' '%s' '%s' '-name' '%s'" unix_socket_path nbd_device export_name;
+        let stdout, stderr = Forkhelpers.execute_command_get_output "/usr/sbin/nbd-client" ["-unix"; unix_socket_path; nbd_device; "-name"; export_name] in
+        debug "XXXX stdout='%s' stderr='%s'" stdout stderr
+      )
+  in
+  debug "XXXX finding free NBD device";
+  let nbd_device = find_free_nbd_device () in
+  debug "XXXX starting NBD client with %s" nbd_device;
+  run_nbd_client ~nbd_device;
+  debug "XXXX started NBD client with %s" nbd_device;
+  nbd_device
+
+let stop_nbd_client ~nbd_device =
+  debug "XXXX stop_nbd_client %s" nbd_device;
+  print_fork_error (fun () -> Forkhelpers.execute_command_get_output "/usr/sbin/nbd-client" ["-disconnect"; nbd_device]) |> ignore
+
 let with_disk ~xc ~xs task disk write f = match disk with
-  | Local path -> f path
+  | Local path ->
+      debug "XXXX local %s" path;
+      f path
   | VDI path ->
+    debug "XXXX vdi %s" path;
     let open Storage_interface in
     let open Storage in
     let sr, vdi = get_disk_by_name task path in
@@ -315,7 +375,14 @@ let with_disk ~xc ~xs task disk write f = match disk with
          let device = create_vbd_frontend ~xc ~xs task frontend_domid vdi in
          finally
            (fun () ->
-              device |> block_device_of_vbd_frontend |> f
+              let block_device = device |> block_device_of_vbd_frontend in
+              debug "XXXX block_device %s" block_device;
+              let nbd_prefix = "hack|nbd:unix:" in
+              let is_nbd = String.startswith nbd_prefix block_device in
+              let unix_socket_path = block_device |> Astring.String.cuts ~empty:false ~sep:nbd_prefix |> List.hd |> String.split_on_char '|' |> List.hd in
+              let nbd_device = if is_nbd then start_nbd_client ~unix_socket_path ~export_name:"qemu_node" else "" in
+              finally (fun () -> f (if is_nbd then nbd_device else block_device))
+                (fun () -> if is_nbd then stop_nbd_client ~nbd_device)
            )
            (fun () ->
               destroy_vbd_frontend ~xc ~xs task device
